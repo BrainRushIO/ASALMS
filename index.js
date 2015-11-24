9b8fb062ed388d222957e9f6ab553cccd519c54b
paleonluna <paleonluna@gmail.com>
2015-11-23 19:55:52 -0800

    fixed bodyparser mispell

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ var express = require( 'express');
 var bodyparser = require('body-parser');
 var app = express();
 
-app.use(bodyParser.json()); // support json encoded bodies
-app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+app.use(bodyparser.json()); // support json encoded bodies
+app.use(bodyparser.urlencoded({ extended: true })); // support encoded bodies
 
 app.get( '/', function( req,res ) 
 {
